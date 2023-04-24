0d422f403eca24e3caee01ba1f584865b95b11aa
Kran3o <126693742+Kran3o1986@users.noreply.github.com>
2023-04-23 23:15:31 -0400

    commit 1 gh pages

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+function capturarDatosComunicar() {
+    var nombre = $("#nombreComunicar").val();
+    var correo = $("#correoComunicar").val();
+    alert("Hola " + nombre + ", Hemos recibido tu mensaje y pronto te responderemos al siguiente correo: " + correo);
+    window.location.href = "index.html";
+  }
+  
+  function capturarDatosReservar() {
+    var nombre = $("#nombreReservar").val();
+    var correo = $("#correoReservar").val();
+    var asistentes = $("#asistentesReservar").val();
+    if (asistentes > 1){
+    alert("Estima@ " + nombre + ", gracias por reservar con nosotros.\nHemos registrado " + asistentes + " asistentes.\nEnviamos el codigo de confirmacion al siguiente correo: " + correo);
+  } else {
+    alert("Estima@ " + nombre + ", gracias por reservar con nosotros.\nHemos registrado " + asistentes + " asistente.\nEnviamos el codigo de confirmacion al siguiente correo: " + correo);
+    window.location.href = "index.html";  
+}
+  }
+
+function mostrarOcultarComunicar() {
+    if (window.matchMedia("(min-width: 1px) and (max-width: 991px)").matches) {
+    document.getElementById('form-reservar').style.display = "none";
+    document.getElementById('form-comunicar').style.display = "block";
+}
+}
+
+function mostrarOcultarReservar() {
+    if (window.matchMedia("(min-width: 1px) and (max-width: 991px)").matches) {
+    document.getElementById('form-comunicar').style.display = "none";
+    document.getElementById('form-reservar').style.display = "block";
+}
+}
+
+function mostrarComunicar() {
+    document.getElementById('form-comunicar').style.display = "block";
+}
+
+function mostrarReservar() {
+    document.getElementById('form-reservar').style.display = "block";
+}
+
+function ocultarComunicar() {
+    document.getElementById('form-comunicar').style.display = "none";
+}
+
+function ocultarReservar() {
+    document.getElementById('form-reservar').style.display = "none";
+}
+
+/*
+function achicarImg() {
+    document.getElementById("bebidas") .style.width= "20%";
+  }
+  
+  function agrandarImg() {
+    document.getElementById("bebidas") .style.width= "100%";
+  }
+  
+  function achicarImg() {
+    document.getElementById("cervezas") .style.width= "20%";
+  }
+  
+  function agrandarImg() {
+    document.getElementById("cervezas") .style.width= "100%";
+  }
+  
+  function achicarImg() {
+    document.getElementById("tablas") .style.width= "20%";
+  }
+  
+  function agrandarImg() {
+    document.getElementById("tablas") .style.width= "100%";
+  }
+  */
+
+  
